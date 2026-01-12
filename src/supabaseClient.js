d080b94f8da872bfb34aa69f3d0f641d86630139
BonfireCode <rodrigoarcanjo690@gmail.com>
2026-01-12 15:29:06 -0300

    Refatorando cliente supabase e add env vars

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://msrxijlattoajnfomlbw.supabase.co'
-const supabaseKey = 'sb_publishable_tMb8fJpVvkmc9ZwLVkl3Ew_UIqvTQQE'
+// Agora ele busca os valores do arquivo .env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey)  
+export const supabase = createClient(supabaseUrl, supabaseKey)
